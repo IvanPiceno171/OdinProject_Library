81024382c7375f781a9770895e34ff95366efe95
unknown <you@example.com>
2022-10-20 11:27:51 -0700

    basic layout

--- OdinProject/main.js
+++ OdinProject/main.js
@@ -1,8 +1,7 @@
-
 let html = document.querySelector(".container");
 const titleInput = document.querySelector("#title");
-const addBtn = document.getElementById('addBooksBtn')
-const form = document.querySelector('.formHTML')
+const addBtn = document.getElementById("addBooksBtn");
+const form = document.querySelector(".formHTML");
 let myLibrary = [];
 
 // function Book() {
@@ -11,10 +10,10 @@ let myLibrary = [];
 
 // class constructor
 class Book {
-    constructor({title, author, year}) {
-        title;
-        author;
-         year;
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
     }
 }
 
@@ -22,36 +21,40 @@ const book1 = new Book("Book One", "John Doe", "2020");
 const book2 = new Book("Book Two", "Jane Doe", "1920");
 
 function addBookToLibrary(newBook) {
-  // do stuff here
+    // do stuff here
     myLibrary.push(newBook);
 }
 
 // addBookToLibrary(book1);
 // addBookToLibrary(book2);
-        
-    
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            let value = new Book(titleInput.value, form.author.value)
-            console.log(value)
-           addBookToLibrary(value)
-        //    addCards()
 
-        })
-        
-        function addCards(){
-            myLibrary.forEach((key) => {
-                // html.innerHTML = '';
-                let newHtml = '';
-                newHtml +=  `
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let value = new Book(titleInput.value, form.author.value);
+    // console.log(value);
+    if(titleInput.value ){
+        alert('bad')
+    }
+    addBookToLibrary(value);
+});
+function addCards() {
+    myLibrary.forEach((key) => {
+        // html.innerHTML = '';
+        let newHtml = "";
+        newHtml += `
                 <div class = "rendered">
+                <ul>
                 <li class='renderedLi'>${key.title}</li>
                         <li class='renderedLi'>${key.author}</li>
                         <li class='renderedLi'>${key.year}</li>
-                        </div>  `
+                        </ul>
+                        </div>  `;
 
-                        html.innerHTML += newHtml
-                    
-                    })
-                }
-        
+        html.innerHTML += newHtml;
+    });
+}
+
+addBtn.addEventListener("click", () => {
+    addCards()
+//    console.log('hello')
+});
